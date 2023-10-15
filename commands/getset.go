8e2effe2eee8cb903d9c4b4c08d27e90f7c59707
Piratey7007 <piratey7007@runbox.com>
2023-10-15 12:47:16 -0500

    gRPC error codes

---
+++ commands/getset.go
@@ -0,0 +1,25 @@
+package commands
+
+import (
+  "github.com/piratey7007/rediss/resp"
+)
+
+func init() {
+  Registry.Register("getset", getset)
+}
+
+func getset(args []resp.Value) resp.Value {
+  if len(args) != 2 {
+    return resp.Value{Typ: "error", Str: "ERR wrong number of arguments for 'getset' command"}
+  }
+
+  key := args[0].Bulk
+  value := args[1].Bulk
+
+  SETsMu.Lock()
+  prev := SETs[key]
+  SETs[key] = value
+  SETsMu.Unlock()
+
+  return resp.Value{Typ: "bulk", Bulk: prev}
+}
