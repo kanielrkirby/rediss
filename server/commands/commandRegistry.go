70b9ae0ef3e8d19c5f4d37a9454241bf92cc52a9
Piratey7007 <piratey7007@runbox.com>
2023-10-21 12:05:55 -0500

    Moved globals to commandRegistry

--- server/commands/commandRegistry.go
+++ server/commands/commandRegistry.go
@@ -5,10 +5,18 @@ import (
 	"encoding/json"
 	"os"
 	"strings"
+  "sync"
 
 	"github.com/piratey7007/rediss/lib/resp"
   "github.com/piratey7007/rediss/lib/utils"
 )
+
+var SETs = map[string]string{}
+var SETsMu = sync.RWMutex{}
+var HSETs = map[string]map[string]string{}
+var HSETsMu = sync.RWMutex{}
+var SUBs = map[string][]chan resp.Value{}
+var SUBsMu = sync.RWMutex{}
 
 type CommandMetadata struct {
 	Name       string
