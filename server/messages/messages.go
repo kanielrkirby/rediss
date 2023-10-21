d1cf03125b3a65eeecc0e6371e7433c9e3c0762a
Piratey7007 <piratey7007@runbox.com>
2023-10-21 10:11:05 -0500

    Updated message to be simpler

--- server/messages/messages.go
+++ server/messages/messages.go
@@ -5,41 +5,26 @@ import (
 )
 
 type Options struct {
-	Version    string
-	Bits       string
-	Commit     string
-	Modified   string
-	Pid        string
 	Port       string
-	ConfigFile bool
 }
 
 func RpStartup(options Options) string {
 	var builder CustomBuilder
 
 	prefix := func() string {
-		return options.Pid + ": " + time.Now().Format("02 Jan 2006 15:04:05.000")
-	}
-
-	configLine := func() string {
-		if options.ConfigFile {
-			return prefix() + " # Warning: no config file specified, using the default config. In order to specify a config file use redis-server"
-		} else {
-			return prefix() + " # Warning: no config file specified, using the default config. In order to specify a config file use redis-server"
-		}
+		return "RedissServer: " + time.Now().Format("02 Jan 2006 15:04:05.000")
 	}
 
 	builder.WriteString(prefix() + " * oO0OoO0OoO0Oo Redis is starting oO0OoO0OoO0Oo")
 	builder.WriteFString("%s * Redis version=%s, bits=%s, commit=%s, modified=%s, options.Pid=%s, just started", prefix(), options.Version, options.Bits, options.Commit, options.Modified, options.Pid)
-	builder.WriteString(configLine())
 
 	builder.WriteString("                _._")
 	builder.WriteString("           _.-``__ ''-._")
-	builder.WriteFString("      _.-``    `.  `_.  ''-._           Redis %s %s %s bit", options.Version, options.Bits, options.Commit)
+	builder.WriteString("      _.-``    `.  `_.  ''-._           Rediss Clone")
 	builder.WriteString("  .-`` .-```.  ```\\/    _.,_ ''-._")
 	builder.WriteString(" (    '      ,       .-`  | `,    )     Running in standalone mode")
 	builder.WriteFString(" |`-._`-...-` __...-.``-._|'` _.-'|     Port: %s", options.Port)
-	builder.WriteFString(" |    `-._   `._    /     _.-'    |     PID: %s", options.Pid)
+	builder.WriteString(" |    `-._   `._    /     _.-'    |")
 	builder.WriteString("  `-._    `-._  `-./  _.-'    _.-'")
 	builder.WriteString(" |`-._`-._    `-.__.-'    _.-'_.-'|")
 	builder.WriteString(" |    `-._`-._        _.-'_.-'    |           https://redis.io")
